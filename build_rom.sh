ec4a89792a84614a8a4ed7c9a212412903fd4adf
UdyneO2 <dynemuhamad@gmail.com>
2024-10-14 16:34:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,11 @@
 # sync rom
+sudo apt update
+sudo apt upgrade
 curl https://storage.googleapis.com/git-repo-downloads/repo > repo
 chmod a+x repo
 sudo install repo /usr/local/bin
 rm repo
+
 
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk
@@ -10,9 +13,11 @@ sudo apt-get install openjdk-8-jre
 
 -- Install build tools
 sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip schedtool
-
+mkdir nusan
+cd nusan
+git config --global user.email "dynemuhamad@gmail.com"
+  git config --global user.name "UdyneO2"
 repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 10
-
 repo init -u https://github.com/Exynos3475/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync --force-sync -j$( nproc --all )
 
